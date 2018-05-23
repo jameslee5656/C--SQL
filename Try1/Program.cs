9882c8a23442b4284f24f68922bc2b8535cda172
james <james.lee56566@gmail.com>
2018-05-23 22:52:23 +0800

    Happended "System.ArgumentException: 'Option not supported.'" problem

---
+++ Try1/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+
+namespace Try1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DBConnect dataBase = new DBConnect();
+            dataBase.Insert();
+            Console.ReadLine();
+        }
+    }
+
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string password;
+        private string uid;
+        private string port;
+        private string charset;
+
+
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "192.168.43.38";
+            port = "3306";
+            charset = "utf8";
+            database = "arduinoData";
+            uid = "monitor";
+            password = "nykd54";
+            string connectionString;
+            connectionString = "Data Source=" + server + ";" +
+                "PORT=" + port + ";" +
+                "USERID=" + uid + ";" +
+                "PASSWORD" + password + ";" +
+                "DATABASE=" + database + ";";
+                //"CHARSET=" + charset +";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Debug.WriteLine("Cannot connect to server. Contact administrator");
+                        break;
+                    case 1045:
+                        Debug.WriteLine("Invalid username/password, please try again");
+                        break;
+                    default:
+                        Debug.WriteLine(ex.Message);
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch(MySqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert()
+        {
+            string query = "INSERT INTO tableinfo (temperature) VALUES ('33')";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        //Update statement
+        public void Update()
+        {
+            string query = "UPDATE tableinfo SET name='Joe' , age='22' WHERE name='John Smith'";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = query;
+                cmd.Connection = connection;
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+
+        }
+
+        //Delete statement
+        public void Delete()
+        {
+            string query = "DELETE FROM tableinfo WHERE name='John Smith'";
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        //Select statement
+        public List<string>[] Select()
+        {
+            string query = "SELECT * FROM tableinfo";
+
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    list[0].Add(dataReader["id"] + "");
+                    list[1].Add(dataReader["name"] + "");
+                    list[2].Add(dataReader["age"] + "");
+                }
+
+                dataReader.Close();
+
+                this.CloseConnection();
+
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+
+        //Count statement
+        public int Count()
+        {
+            string query = "SELECT Count(*) FROM tableinfo";
+            int Count = -1;
+
+            if(this.OpenConnection()==true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                this.CloseConnection();
+
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
+        }
+
+        //Backup
+        public void Backup()
+        {
+        }
+
+        //Restore
+        public void Restore()
+        {
+        }
+    }
+
+}
